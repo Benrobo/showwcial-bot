a4f1b5b60a7f9d0aa9616b7d4417e5b412e9741c
benrobo <alumonabenaiah71@gmail.com>
2023-05-19 12:43:32 +0100

    updated env

--- config/env.js
+++ config/env.js
@@ -5,6 +5,10 @@ const Env = {
   discordClientId: process.env.DISCORD_CLIENT_ID,
   discordToken: process.env.DISCORD_TOKEN,
   botId: "1100925767740817428",
+  backendApi:
+    process.env.NODE_ENV !== "production"
+      ? "http://localhost:3000/api"
+      : process.env.ShowwcialBackend,
 };
 
 module.exports = Env;
