406caaaf7ad152831a0ea36eb58e8201e6dd9dd6
benrobo <alumonabenaiah71@gmail.com>
2023-05-19 16:33:31 +0100

    added keep alive script

---
+++ keep_alive.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3421;
+
+app.get("/", (req, res) => {
+  return res.status(200).send(`✅ Showwcial-Bot is online.`);
+});
+
+app.listen(port, () => console.log(`Bot server listening on ${port}`));
